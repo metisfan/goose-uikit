83df35b8340c10a6ff497ffd11a519d03d22d2f5
Mona Lisa <you@example.com>
2022-04-04 00:16:08 +0200

    33

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2215,7 +2215,7 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/rickandmortydefi",
+                href: "https://t.me/yieldboosterfinance",
             },
             // {
             //   label: "Bahasa Indonesia",
@@ -2270,7 +2270,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/RickAndMortyFin",
+        href: "https://twitter.com/yieldboosterfin",
     },
     // {
     //     label: "Reddit",
